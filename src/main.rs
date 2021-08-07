050198e6040f1e8f27b7a846ed8221ec65f22b07
ryo <ryo7000@gmail.com>
2021-08-07 12:33:36 +0900

    Add dotenv integ

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+type StdErr = Box<dyn std::error::Error>;
+
+fn main() -> Result<(), StdErr> {
+    // loads env variables from .env
+    dotenv::dotenv()?;
+
+    assert_eq!("INFO", std::env::var("LOG_LEVEL").unwrap());
+
+    Ok(())
 }
