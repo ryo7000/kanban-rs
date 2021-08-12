b885d1d1853e2e3349e1b3825c3b72ddb54b9280
ryo <ryo7000@gmail.com>
2021-08-12 20:27:07 +0900

    Add root response

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+// required for rocket macros to work
+#![feature(proc_macro_hygiene, decl_macro)]
+
 #[macro_use]
 extern crate diesel;
 
@@ -8,10 +11,19 @@ mod schema;
 
 type StdErr = Box<dyn std::error::Error>;
 
+#[rocket::get("/")]
+fn hellow_world() -> &'static str {
+    "Hello, world!"
+}
+
 fn main() -> Result<(), StdErr> {
     // loads env variables from .env
     dotenv::dotenv()?;
     logger::init()?;
+
+    rocket::ignite()
+        .mount("/", rocket::routes![hellow_world])
+        .launch();
 
     Ok(())
 }
