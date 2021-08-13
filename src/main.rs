a855023f80a642afdb1821cac4e7acdbaf2d3412
ryo <ryo7000@gmail.com>
2021-08-13 12:23:41 +0900

    Add axum and root route

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
+use axum::{http::StatusCode, prelude::*, response::IntoResponse};
+use std::net::SocketAddr;
+
 mod db;
 mod logger;
 mod models;
 
 type StdErr = Box<dyn std::error::Error>;
 
-fn main() -> Result<(), StdErr> {
+async fn hello_world() -> &'static str {
+    "Hello, world!"
+}
+
+#[tokio::main]
+async fn main() -> Result<(), StdErr> {
     // loads env variables from .env
     dotenv::dotenv()?;
     logger::init()?;
+
+    let app = route("/", get(hello_world));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await?;
 
     Ok(())
 }
