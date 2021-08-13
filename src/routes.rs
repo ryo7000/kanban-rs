522aadea7d095e684cf53caf62a2781119deeac6
ryo <ryo7000@gmail.com>
2021-08-13 18:07:41 +0900

    Add routes

--- src/routes.rs
+++ src/routes.rs
@@ -1,14 +1,71 @@
 use crate::db;
+use crate::models;
 use axum::{
-    extract::Extension, prelude::*, response::IntoResponse, routing::BoxRoute, AddExtensionLayer,
+    extract::{Extension, Json, Path},
+    http::StatusCode,
+    prelude::*,
+    response::IntoResponse,
+    routing::BoxRoute,
+    AddExtensionLayer,
 };
+
+// board routes
 
 async fn boards(Extension(db): Extension<db::Db>) -> impl IntoResponse {
     response::Json(db.boards().await.unwrap())
 }
 
+async fn create_board(
+    Json(input): Json<models::CreateBoard>,
+    Extension(db): Extension<db::Db>,
+) -> impl IntoResponse {
+    response::Json(db.create_board(input).await.unwrap())
+}
+
+async fn board_summary(Path(id): Path<i64>, Extension(db): Extension<db::Db>) -> impl IntoResponse {
+    response::Json(db.board_summary(id).await.unwrap())
+}
+
+async fn delete_board(Path(id): Path<i64>, Extension(db): Extension<db::Db>) -> impl IntoResponse {
+    db.delete_board(id).await.unwrap();
+
+    StatusCode::NO_CONTENT
+}
+
+// card routes
+
+async fn cards(Path(id): Path<i64>, Extension(db): Extension<db::Db>) -> impl IntoResponse {
+    response::Json(db.cards(id).await.unwrap())
+}
+
+async fn create_card(
+    Json(input): Json<models::CreateCard>,
+    Extension(db): Extension<db::Db>,
+) -> impl IntoResponse {
+    response::Json(db.create_card(input).await.unwrap())
+}
+
+async fn update_card(
+    Path(id): Path<i64>,
+    Json(input): Json<models::UpdateCard>,
+    Extension(db): Extension<db::Db>,
+) -> impl IntoResponse {
+    response::Json(db.update_card(id, input).await.unwrap())
+}
+
+async fn delete_card(Path(id): Path<i64>, Extension(db): Extension<db::Db>) -> impl IntoResponse {
+    db.delete_card(id).await.unwrap();
+
+    StatusCode::NO_CONTENT
+}
+
 pub fn root(db: db::Db) -> BoxRoute<Body> {
-    route("/boards", get(boards))
+    route("/boards", get(boards).post(create_board))
+        .route("/boards/:id", delete(delete_board))
+        .route("/boards/:id/summary", get(board_summary))
+        .route("/boards/:id/cards", get(cards))
+        .route("/cards", post(create_card))
+        .route("/cards/:id", patch(update_card).delete(delete_card))
         .layer(AddExtensionLayer::new(db))
         .boxed()
 }
